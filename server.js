6e4c35b2e50671119095c900a424945639ecda8a
code4fun0x1 <shashank0x1@gmail.com>
2017-04-07 23:13:23 +0530

    for shikhar

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fs=require('fs');
+//const fs=require('fs');
 
 //before deploying our app we need to set out port dynamically
 //we use environment variable for it
@@ -14,33 +14,33 @@ var app = express();
 //now we set up hbs which is view eenginee for express
 //based in Express.js
 
-const hbs = require('hbs');
+//const hbs = require('hbs');
 
 //add support for partials in hbs
-hbs.registerPartials(__dirname+'/views/partials');
+//hbs.registerPartials(__dirname+'/views/partials');
 
 //set expess to use hbs view engine
-app.set('view engine', 'hbs');
+//app.set('view engine', 'hbs');
 //now make  a directory 'views'
 //eexpress uses it as a default directory for template
 
 //app.use() to register a middleware
-app.use((req,res,next)=>{
-  var now=new Date().toString();
-  var log=`${now} ${req.method} ${req.url}`;
-  console.log(log);
-  fs.appendFile('server.log',log+'\n');
-  next();
-});
+// app.use((req,res,next)=>{
+//   var now=new Date().toString();
+//   var log=`${now} ${req.method} ${req.url}`;
+//   console.log(log);
+//   fs.appendFile('server.log',log+'\n');
+//   next();
+// });
 //set up middleware for the static pages
 app.use(express.static(__dirname + '/public'));
-hbs.registerHelper('getCurrentYear',()=>{
-  return new Date().getFullYear();
-});
-
-hbs.registerHelper('screamIt',(text)=>{
-  return text.toUpperCase();
-});
+// hbs.registerHelper('getCurrentYear',()=>{
+//   return new Date().getFullYear();
+// });
+//
+// hbs.registerHelper('screamIt',(text)=>{
+//   return text.toUpperCase();
+// });
 
 // step 2 SETUP THE HANDLER
 // '/' for the root url
@@ -57,16 +57,16 @@ hbs.registerHelper('screamIt',(text)=>{
 //     });
 // });
 
-app.get('/', (req, res) => {
-    //res.send('Hello Express');
-    //res.send('<h1>Hello Express</h1>');
-    res.render('home.hbs',{
-        pageTitle: 'Home',
-        content: 'Welcome to home page',
-        //revoved beacause we are now using hbs helper
-        //currentYear:new Date().getFullYear()
-    });
-});
+// app.get('/', (req, res) => {
+//     //res.send('Hello Express');
+//     //res.send('<h1>Hello Express</h1>');
+//     res.render('home.hbs',{
+//         pageTitle: 'Home',
+//         content: 'Welcome to home page',
+//         //revoved beacause we are now using hbs helper
+//         //currentYear:new Date().getFullYear()
+//     });
+// });
 
 //old now reeplaced with HBSS template engine
 //app.get('/about',(req,res)=>{
@@ -74,27 +74,27 @@ app.get('/', (req, res) => {
 //})
 
 
-app.get('/about', (req, res) => {
-    //send key valur paires to fill templatee with actual data
-    res.render('about.hbs', {
-        pageTitle: 'About Page',
-        //revoved beacause we are now using hbs helper
-        //currentYear:new Date().getFullYear()
-    });
-});
+// app.get('/about', (req, res) => {
+//     //send key valur paires to fill templatee with actual data
+//     res.render('about.hbs', {
+//         pageTitle: 'About Page',
+//         //revoved beacause we are now using hbs helper
+//         //currentYear:new Date().getFullYear()
+//     });
+// });
 
 
 
-app.get('/bad', (req, res) => {
-    res.send({
-        errorMessage: 'Cannot Find the Page'
-    });
-})
+// app.get('/bad', (req, res) => {
+//     res.send({
+//         errorMessage: 'Cannot Find the Page'
+//     });
+// })
 
 //TEST FOR COMPLETE LYFECYCLE
-app.get('/projects',(req,res)=>{
-  res.render('projects.hbs',{pageTitle:'Projects'});
-});
+// app.get('/projects',(req,res)=>{
+//   res.render('projects.hbs',{pageTitle:'Projects'});
+// });
 
 
 
